3405d2e9749175e2158cca141aa52646e1e2e0bc
ansh20202020 <anshuman.gem@gmail.com>
2025-11-23 14:13:12 +0530

    adding 04 and 05

--- 03-Tailwind/src/App.jsx
+++ 03-Tailwind/src/App.jsx
@@ -3,13 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Example from './Example.jsx'
+import Card from "./components/Card.jsx"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
    <>
-    <h1 className='bg-green-400 text-black p-4 rounded-4xl'>Test</h1>
+    {/* <h1 className='bg-green-400 text-black p-4 rounded-4xl'>Test</h1> */}
+    
+    <div className="flex flex-wrap gap-6 justify-center">
+        <Card Title="Top" btnText="Explore Further"/>
+        <Card Title="Featured" />
+        <Card />
+        <Card />
+    </div>
+
     {/* <Example/> */}
    </>
   )
