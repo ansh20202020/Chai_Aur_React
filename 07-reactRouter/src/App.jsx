7bccd9bcc696d7e357a516d2ab5265aa71754fd4
anshuman302004 <anshuman302004@gmail.com>
2025-11-24 19:03:31 +0000

    half 07

---
+++ 07-reactRouter/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
+
+function App() {
+
+  return (
+    <>
+      {/* <h1 className='text-center bg-green-600 p-4 text-black'>
+        React Router
+      </h1> */}
+
+      <Header />
+      <Home />
+      <Footer />
+    </>
+  )
+}
+
+export default App
